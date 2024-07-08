cdf99917ab8e789271166b159a26e862c376f121
Saurabh Yadav <quic_sauyad@quicinc.com>
2024-07-08 12:34:27 +0530

    display-intf: fix license text
    Change-Id: Idda215f0fc86a1f811e54ed9c5d0ce3e6ba82c74

--- sdmclient/sdm_display_intf_aiqe.h
+++ sdmclient/sdm_display_intf_aiqe.h
@@ -1,5 +1,4 @@
 /*
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
  * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
